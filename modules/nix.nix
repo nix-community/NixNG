0a581a6973ff312fe27a3d6f47b859ecaba2be0e
Magic_RB <magic_rb@redalder.org>
2021-04-29 20:39:45 +0200

    Nix module additions
    Signed-off-by: Magic_RB <magic_rb@redalder.org>

--- modules/nix.nix
+++ modules/nix.nix
@@ -2,6 +2,34 @@
 with lib;
 let
   cfg = config.nix;
+
+  nix = cfg.package.out;
+
+  nixVersion = getVersion nix;
+
+  isNix23 = versionAtLeast nixVersion "2.3pre";
+
+  parser =
+    let
+      valToString = v:
+        if isString v then
+          "${v}"
+        else if isInt v then
+          "${toString v}"
+        else if isBool v then
+          if v then
+            "true"
+          else
+            "false"
+        else if isList v then
+          concatMapStringsSep " " (x: valToString x) v
+        else
+          abort "Invalid config, module system should have caught this!";
+    in
+      config:
+      concatStringsSep "\n" (mapAttrsToList (n: v:
+        "${n} = ${valToString v}"
+      ) config);
 in
 {
   options.nix = {
@@ -25,6 +53,31 @@ in
       default = 32;
     };
 
+    config = mkOption {
+      description = ''
+        Contents of <literal>nix.conf</literal>, represented using an attrset containing strings, bools, ints, or lists of strings, bools, or ints.
+      '';
+      type = with types; attrsOf (oneOf [ str int bool (listOf (oneOf [ str int bool ]))]);
+      example =
+        {
+          sandbox = true;
+          require-sigs = true;
+          cores = 0;
+        };
+      default = {};
+      apply = x:
+        builtins.toFile "nix.conf" (parser x);
+    };
+
+    loadNixDb = mkOption {
+      description = ''
+        Whether to create the registration of this closure, and to load it at
+        activation time. Useful when bootstraping a system, such as containers.
+      '';
+      type = types.bool;
+      default = false;
+    };
+    
     nixPath = mkOption {
       description = ''
         The Nix Path, basically channels.
@@ -35,44 +88,28 @@ in
       ];
     };
 
-    readOnlyStore = mkOption {
+    overlayNix = mkOption {
       description = ''
-        Whether the store should be made read-only by using a bind mount.
+        Will overlay the included <literal>/nix</literal> and the one at the
+        path in this option, with the second one being the upper dir. Can be
+        used to persist <literal>/nix</literal> across container restarts.
+      '';
+      type = with types; nullOr str;
+      default = null;
+    };
+
+    daemon = mkOption {
+      description = ''
+        Whether to start the Nix daemon, therefore run in multi-user or single-user mode.
       '';
       type = types.bool;
-      default = true;
-      apply = x:
-        if x then
-          ''
-            export PATH=${pkgs.busybox}/bin:${pkgs.utillinux}/bin 
-
-            # Borrowed from NixOS:
-            # https://github.com/NixOS/nixpkgs/blob/08546595671398eb7939eb5865d111351c546cf1/nixos/modules/system/boot/stage-2-init.sh
-
-            # Make /nix/store a read-only bind mount to enforce immutability of
-            # the Nix store.  Note that we can't use "chown root:nixbld" here
-            # because users/groups might not exist yet.
-            # Silence chown/chmod to fail gracefully on a readonly filesystem
-            # like squashfs.
-            chown -f 0:30000 /nix/store
-            chmod -f 1775 /nix/store
-            if [ -n "@readOnlyStore@" ]; then
-              if ! [[ "$(findmnt --noheadings --output OPTIONS /nix/store)" =~ ro(,|$) ]]; then
-                # FIXME when linux < 4.5 is EOL, switch to atomic bind mounts
-                #mount /nix/store /nix/store -o bind,remount,ro
-                mount --bind /nix/store /nix/store
-                mount -o remount,ro,bind /nix/store
-              fi
-            fi
-          ''
-        else
-          "true";
+      default = false;
     };
   };
 
   config = mkIf cfg.enable {
-    system.createNixRegistration = true;
-    system.activation.loadNixDb = 
+    system.createNixRegistration = mkIf cfg.loadNixDb true;
+    system.activation.loadNixDb = mkIf cfg.loadNixDb
       (nglib.dag.dagEntryAfter [ "currentSystem" "users" ]
         ''
           export PATH=${pkgs.busybox}/bin:${cfg.package}/bin
@@ -82,6 +119,17 @@ in
             nix-store --load-db < /run/current-system/registration
           fi
         '');
+
+    system.activation.overlayNix = mkIf (cfg.overlayNix != null)
+      (nglib.dag.dagEntryBefore [ "loadNixDb" ]
+        ''
+          export PATH=${pkgs.busybox}/bin
+
+          mkdir -p ${cfg.overlayNix}/upper ${cfg.overlayNix}/work
+          chmod 000 ${cfg.overlayNix}/work
+          ${pkgs.fuse-overlayfs}/bin/fuse-overlayfs -o lowerdir=/nix,upperdir=${cfg.overlayNix}/upper,workdir=${cfg.overlayNix}/work /nix
+        '');
+      
 
     users = {
       users = mkMerge (map (x:
@@ -102,7 +150,53 @@ in
 
     environment.variables = {
       NIX_PATH = cfg.nixPath;
+      NIX_REMOTE = mkIf cfg.daemon "daemon";
+    };
+
+    nix.config = mkDefault {
+      build-users-group = "nixbld";
+      max-jobs = "auto";
+      cores = 0;
+      sandbox = true;
+      extra-sandbox-paths = [];
+      substituters = [ "https://cache.nixos.org/" ];
+      trusted-substituters = [];
+      trusted-public-keys =
+        [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+      auto-optimise-store = false;
+      require-sigs = true;
+      allowed-users = "*";
+      builders = [];
+
+      system-features =  mkDefault (
+        [ "nixos-test" "benchmark" "big-parallel" "kvm" ] ++
+        optionals (pkgs.hostPlatform.platform ? gcc.arch) (
+          # a builder can run code for `platform.gcc.arch` and inferior architectures
+          [ "gccarch-${pkgs.hostPlatform.platform.gcc.arch}" ] ++
+          map (x: "gccarch-${x}") lib.systems.architectures.inferiors.${pkgs.hostPlatform.platform.gcc.arch}
+        )
+      );
+
+      sandbox-fallback = mkIf isNix23 false;
+    };
+
+    system.activation.nix = nglib.dag.dagEntryAnywhere ''
+      export PATH=${pkgs.busybox}/bin
+
+      install -m 0755 -d /nix/var/nix/{gcroots,profiles}/per-user
+
+      # NixOS canonical location + Debian/Ubuntu/Arch/Gentoo compatibility.
+      mkdir -m 0755 -p /etc/nix
+      ln -sfn ${cfg.config} /etc/nix/.nix.conf.tmp 
+      mv /etc/nix/.nix.conf.tmp /etc/nix/nix.conf # atomically replace /etc/nix/nix.conf
+    '';
+
+    init.services.nix-daemon = mkIf cfg.daemon {
+      script = pkgs.writeShellScript "nix-daemon" ''
+        chpst -b nix-daemon ${cfg.package}/bin/nix-daemon --daemon
+      '';
+      enabled = true;
     };
   };
 }
- 
+  
