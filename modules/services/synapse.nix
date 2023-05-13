e5c3b6a5685d75fe2e55ddf7828f739ef599b68a
Magic_RB <magic_rb@redalder.org>
2023-05-13 21:11:07 +0200

    Revert "synapse: concatenate arguments with newlines"
    This reverts commit 3af8afeb8cb121cffecabef360a3881b242abe40.

--- modules/services/synapse.nix
+++ modules/services/synapse.nix
@@ -103,8 +103,8 @@ in
                 pkgs.writeShellScript "synapse-worker-${n}.sh"
                   ''
                     ${pkgs.matrix-synapse}/bin/synapse_worker \
-                      ${concatStringsSep "\n" (mapAttrsToList (n: v: if isList v then concatMapStringsSep "\n" (x: "--${n} \"${x}\"") v else "\"--${n} ${v}\"") v.arguments)}
-                 '';
+                      ${concatStringsSep " " (mapAttrsToList (n: v: if isList v then concatMapStringsSep " " (x: "--${n} \"${x}\"") v else "\"--${n} ${v}\"") v.arguments)}
+    #             '';
           }
       );
   };
