92583ac09f51f6cac60d2f2b32b5149eb6216aad
magic_rb <magic_rb@redalder.org>
2024-12-26 16:46:01 +0100

    Add `nixos` compat modules
    Signed-off-by: magic_rb <magic_rb@redalder.org>

---
+++ modules/nixos/default.nix
@@ -0,0 +1,39 @@
+{ lib, pkgs, ... }:
+{
+  imports = [
+    ./systemd.nix
+    ./nginx.nix
+    ./users.nix
+    ./postgresql.nix
+    ./assertions.nix
+    ./oauth2-proxy.nix
+    ./nix.nix
+    ./meta.nix
+    ./networking.nix
+    ./buildbot.nix
+  ];
+
+  options.nixos = lib.mkOption {
+    type = lib.types.submodule {
+      _module.args = {
+        inherit pkgs;
+      };
+    };
+    default = { };
+  };
+
+  config.assertions = lib.optionals __enableExperimentalNixOSCompatibility [
+    {
+      assertion = (config.nixos.acceptRisks == "I accept the risks");
+      message = ''
+        NixOS module compatibility is highly experimental, severely unfinished and most definitely has
+        functional and security bugs. Unless you know what you're doing and are willing to accept the risks
+        reconsider it's usage. To signify you are aware of these risks, set the option
+        `config.nixos.acceptRisks` to `"I accept the risks"`.
+
+        If you run into any of the aforementioned deficiencies please reach out on Matrix at
+        `#nixng:matrix.redalder.org`.
+      '';
+    }
+  ];
+}
