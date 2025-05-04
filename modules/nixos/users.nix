48605fa386092e8df874871948e769807c1f4e78
magic_rb <magic_rb@redalder.org>
2025-05-04 15:41:59 +0200

    fmt
    Signed-off-by: magic_rb <magic_rb@redalder.org>

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -3,32 +3,35 @@
   options = {
     nixos.users.users = lib.mkOption {
       type = lib.types.attrsOf (
-        lib.types.submodule ({name, ...}: {
-          options = {
-            description = lib.mkOption { type = lib.types.str; };
-            createHome = lib.mkOption { type = lib.types.bool; };
-            home = lib.mkOption { type = lib.types.path; };
-            group = lib.mkOption { type = lib.types.str; };
-            extraGroups = lib.mkOption {
-              type = lib.types.listOf lib.types.str;
-              default = [ ];
+        lib.types.submodule (
+          { name, ... }:
+          {
+            options = {
+              description = lib.mkOption { type = lib.types.str; };
+              createHome = lib.mkOption { type = lib.types.bool; };
+              home = lib.mkOption { type = lib.types.path; };
+              group = lib.mkOption { type = lib.types.str; };
+              extraGroups = lib.mkOption {
+                type = lib.types.listOf lib.types.str;
+                default = [ ];
+              };
+              useDefaultShell = lib.mkOption { type = lib.types.bool; };
+              isSystemUser = lib.mkOption {
+                type = lib.types.bool;
+                default = true;
+              };
+              isNormalUser = lib.mkOption {
+                type = lib.types.bool;
+                default = false;
+              };
+              name = lib.mkOption {
+                type = lib.types.str;
+                default = name;
+                readOnly = true;
+              };
             };
-            useDefaultShell = lib.mkOption { type = lib.types.bool; };
-            isSystemUser = lib.mkOption {
-              type = lib.types.bool;
-              default = true;
-            };
-            isNormalUser = lib.mkOption {
-              type = lib.types.bool;
-              default = false;
-            };
-            name = lib.mkOption {
-              type = lib.types.str;
-              default = name;
-              readOnly = true;
-            };
-          };
-        })
+          }
+        )
       );
       default = { };
     };
@@ -41,10 +44,18 @@
   };
 
   config = {
-    users.users =
-      (lib.mapAttrs (
-        _: v: lib.filterAttrs (n: _: !lib.elem n [ "name" "isSystemUser" ]) v
-      ) config.nixos.users.users);
+    users.users = (
+      lib.mapAttrs (
+        _: v:
+        lib.filterAttrs (
+          n: _:
+          !lib.elem n [
+            "name"
+            "isSystemUser"
+          ]
+        ) v
+      ) config.nixos.users.users
+    );
     users.groups = config.nixos.users.groups;
   };
 }
