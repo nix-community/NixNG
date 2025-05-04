31035c0b2d226521a4c36f8153ba501e063386b9
magic_rb <magic_rb@redalder.org>
2025-05-04 15:22:02 +0200

    Add `nglib.mkOptionsEqual`
    Signed-off-by: magic_rb <magic_rb@redalder.org>

--- lib/default.nix
+++ lib/default.nix
@@ -102,7 +102,24 @@ lib.fix (
         )
       );
 
-    inherit (import ./options.nix args) mkUserOption mkGroupOption;
+    errorExperimentalNixOS =
+      config:
+      lib.throwIfNot (config.nixos.acceptRisks == "I accept the risks") ''
+        NixOS module compatibility is highly experimental, severely unfinished and most definitely has
+        functional and security bugs. Unless you know what you're doing and are willing to accept the risks
+        reconsider it's usage. To signify you are aware of these risks, set the option
+        `config.nixos.acceptRisks` to `"I accept the risks"`.
+
+        If you run into any of the aforementioned deficiencies please reach out on Matrix at
+        `#nixng:matrix.redalder.org`.
+      '';
+
+    inherit (import ./options.nix args)
+      mkUserOption
+      mkGroupOption
+      mkOptionsEqual
+      getOptionFromPath
+      ;
 
     makeSystem = import ./make-system.nix { inherit lib nglib overlay; };
   }
