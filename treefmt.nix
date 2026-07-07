799506456732f215f76b9d45dcc566ce63781881
magic_rb <magic_rb@redalder.org>
2026-07-07 22:47:08 +0200

    Add `deadnix`

--- treefmt.nix
+++ treefmt.nix
@@ -2,8 +2,15 @@
 {
   projectRootFile = "flake.nix";
   programs.nixfmt.enable = true;
+  programs.deadnix.enable = true;
 
   settings.formatter.nixfmt = {
+    excludes = [ ];
+    includes = [ "*.nix" ];
+    options = [ ];
+  };
+
+  settings.formatter.deadnix = {
     excludes = [ ];
     includes = [ "*.nix" ];
     options = [ ];
